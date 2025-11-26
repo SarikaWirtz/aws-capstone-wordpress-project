2ef73ad84a73e076a1596c766784ffc130ed92b7
Sarika Wirtz <sarikasingh223@gmail.com>
2025-11-26 17:06:49 +0100

    #9:Add autoscaling and loadbalancer

---
+++ alb.tf
@@ -0,0 +1,51 @@
+# Adding Application Load Balancer
+
+resource "aws_lb" "capstone_lb" {
+  name                       = "capstone-lb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.aws_capstone_lb_sg.id]
+  subnets                    = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "CapstoneLoadBalancer"
+  }
+}
+
+# Adding Target Group
+
+resource "aws_lb_target_group" "capstone_tg" {
+  name        = "capstone-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.aws_capstone_vpc.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    matcher             = "200-399"
+    interval            = 60
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+  tags = {
+    Name = "CapstoneTargetGroup"
+  }
+}
+
+# Adding Listener
+resource "aws_lb_listener" "capstone_listener" {
+  load_balancer_arn = aws_lb.capstone_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.capstone_tg.arn
+  }
+}
+
---
+++ autoscaling.tf
@@ -0,0 +1,57 @@
+# Adding Auto Scaling
+
+# Launch Template
+resource "aws_launch_template" "launch-template" {
+  name                   = "wordpress-template"
+  image_id               = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.aws_webserver_sg.id, aws_security_group.aws_ssh_sg.id]
+  user_data              = base64encode(templatefile("${path.module}/userdata.sh", {
+    db_name        = var.db_name
+    username       = var.username
+    password       = var.password
+    DBRootPassword = var.DBRootPassword
+    rds_endpoint = replace(aws_db_instance.mysql.endpoint, ":3306", "") 
+    }))
+  key_name               = "vockey"
+}
+
+resource "aws_autoscaling_group" "autoscaling-group" {
+    depends_on       = [aws_nat_gateway.nat]
+    default_cooldown = 30
+    launch_template {
+        id      = aws_launch_template.launch-template.id
+         version = "$Latest"
+    }
+    desired_capacity = 2 # 1 for testing
+    min_size         = 2 # 1 for testing
+    max_size         = 3 # 2 for testing
+
+    target_group_arns   = [aws_lb_target_group.capstone_tg.arn]
+    vpc_zone_identifier = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+    tag {
+        key                 = "Name"
+        value               = "AutoScale-Wordpress"
+        propagate_at_launch = true
+    }
+}
+
+# Scaling Policy
+resource "aws_autoscaling_policy" "scale-out-policy" {
+    name                      = "scale-out-policy"
+    autoscaling_group_name    = aws_autoscaling_group.autoscaling-group.name
+    policy_type               = "TargetTrackingScaling"
+    estimated_instance_warmup = 30
+    target_tracking_configuration {
+        predefined_metric_specification {
+            predefined_metric_type = "ASGAverageCPUUtilization"
+        }
+        target_value = 50.0 # 20 for testing
+    }
+}
+
+# Group Attachment
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.autoscaling-group.id
+  lb_target_group_arn    = aws_lb_target_group.capstone_tg.arn
+}
