28195f1a0ac0e8d58de1fc703ce35d6038c9e0dc
Sarika Wirtz <sarikasingh223@gmail.com>
2025-11-26 11:24:23 +0100

    #5:Test loadbalancer instance

--- ec2.tf
+++ ec2.tf
@@ -26,21 +26,35 @@ resource "aws_instance" "bastion" {
 
 
 # Create Web Server Instance in Public Subnet 1
-resource "aws_instance" "web_server" {
+# resource "aws_instance" "web_server" {
+#   ami                    = data.aws_ami.amazon_linux_2.id
+#   instance_type          = var.aws_instance_type_t2micro
+#   availability_zone      = var.aws_availability_zone_a
+#   vpc_security_group_ids = [aws_security_group.aws_ssh_sg.id, aws_security_group.aws_webserver_sg.id]
+#   subnet_id              = aws_subnet.public_subnet_1.id
+#   key_name               = "vockey" # Create new key if using different region than us-west-2
+#   user_data              = templatefile("${path.module}/userdata.sh", {
+#     db_name        = var.db_name
+#     username       = var.username
+#     password       = var.password
+#     DBRootPassword = var.DBRootPassword
+#     rds_endpoint   = aws_db_instance.mysql.endpoint
+#   })
+#   tags = {
+#     Name = "AwsCapstoneWebServer"
+#   }
+# }
+
+# # Second Instance for Load Balancing testing -- Will be replaced by Auto Scaling Group
+resource "aws_instance" "web2" {
   ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = var.aws_instance_type_t2micro
-  availability_zone      = var.aws_availability_zone_a
+  availability_zone      = var.aws_availability_zone_b
   vpc_security_group_ids = [aws_security_group.aws_ssh_sg.id, aws_security_group.aws_webserver_sg.id]
-  subnet_id              = aws_subnet.public_subnet_1.id
+  subnet_id              = aws_subnet.public_subnet_2.id
   key_name               = "vockey" # Create new key if using different region than us-west-2
-  user_data              = templatefile("${path.module}/userdata.sh", {
-    db_name        = var.db_name
-    username       = var.username
-    password       = var.password
-    DBRootPassword = var.DBRootPassword
-    rds_endpoint   = aws_db_instance.mysql.endpoint
-  })
+  user_data              = file("loadbalancertest.sh")
   tags = {
-    Name = "AwsCapstoneWebServer"
+    Name = "AwsCapstoneWebServer2"
   }
 }
